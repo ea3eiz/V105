60fce6640136021b71df074449bb2c50dcad0cad
Manel <ea3eiz@ea3eiz.com>
2020-06-17 19:59:52 +0200

    1

--- info_bm.sh
+++ info_bm.sh
@@ -6,31 +6,32 @@ Servidor="Servidor:"
 Memoria="Memoria:"
 informacion="INFORMACIÓN"
 
+DIRECTORIO="MMDVMBM.ini"
+linea="1c"
 
-
-mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/MMDVMBM.ini`
+mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/$DIRECTORIO`
 buscar=":"
 caracteres=`expr index $mode $buscar`
 caracteres_linea=`expr $caracteres - 1`
 numero_linea_port=`expr substr $mode 1 $caracteres_linea`
-mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMBM.ini)
+mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/$DIRECTORIO)
 puerto=`expr substr $mode 11 9`
 puerto="  "$puerto
 
-frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMBM.ini)
+frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/$DIRECTORIO)
 frecuencia=`expr substr $frecuencia 13 17`
 frecuencia=$frecuencia
 
-address=`grep -n -m 1 "^Address=" /home/pi/MMDVMHost/MMDVMBM.ini`
+address=`grep -n -m 1 "^Address=" /home/pi/MMDVMHost/$DIRECTORIO`
 buscar=":"
 caracteres=`expr index $address $buscar`
 caracteres_linea=`expr $caracteres - 1`
 numero_linea_address=`expr substr $address 1 $caracteres_linea`
-mode=$(awk "NR==$numero_linea_address" /home/pi/MMDVMHost/MMDVMBM.ini)
+mode=$(awk "NR==$numero_linea_address" /home/pi/MMDVMHost/$DIRECTORIO)
 address=`expr substr $address 13 35`
 address="  "$address
 
-memoria=$(awk "NR==1" /home/pi/.local/info_memorias)
+memoria=$(awk "NR==$linea" /home/pi/.local/info_memorias)
 
 #Colores
 ROJO="\033[1;31m"
@@ -40,7 +41,7 @@ AMARILLO="\033[1;33m"
 CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
-#figlet $puerto
+
 echo "${BLANCO}"
 echo "  $informacion (BM)"
 echo "${VERDE}"
