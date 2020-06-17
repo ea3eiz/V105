4d8a75f76b68d057bb9106aed42fe05f7289f6b3
ea3eiz <ea3eiz@ea3eiz.com>
2020-06-17 13:07:20 +0200

    1

--- ejecutar_dmrgateway.sh
+++ ejecutar_dmrgateway.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-idioma=$(awk "NR==1" /home/pi/.local/idioma)
-if [ $idioma = English ]; then
-icono=ICONO_CLOSE.png
-icono_NO=ICONO_OPEN.png
-
-else
-icono=ICONO_CERRAR.png
-icono_NO=ICONO_ABRIR.png
-
-fi
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 
@@ -23,7 +13,7 @@ cd /home/pi/Desktop
 sudo cp Abrir_dmrgateway.desktop /home/pi/
 sleep 1
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_dmrgateway.sh'" /home/pi/Abrir_dmrgateway.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/$icono" /home/pi/Abrir_dmrgateway.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRGATEWAY_ON.png" /home/pi/Abrir_dmrgateway.desktop
 sed -i "10c Name[es_ES]=DMR Gateway" /home/pi/Abrir_dmrgateway.desktop
 sed -i "19c DMRGateway=OFF" /home/pi/.local/status.ini
 sleep 1
@@ -54,7 +44,7 @@ cd /home/pi/Desktop
 sudo cp Abrir_dmrgateway.desktop /home/pi/
 sleep 1
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=72x20 -e sudo sh ejecutar_dmrgateway.sh'" /home/pi/Abrir_dmrgateway.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/$icono_NO" /home/pi/Abrir_dmrgateway.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRGATEWAY_OFF.png" /home/pi/Abrir_dmrgateway.desktop
 sed -i "10c Name[es_ES]=Open DMRGateway" /home/pi/Abrir_dmrgateway.desktop
 sed -i "19c DMRGateway=OFF" /home/pi/.local/status.ini
 sleep 1
