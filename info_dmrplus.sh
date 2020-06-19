6a57bbe3f4dd9cdcf3b195b12e700e8b04809536
ea3eiz <ea3eiz@ea3eiz.com>
2020-06-19 13:02:00 +0200

    1

--- info_dmrplus.sh
+++ info_dmrplus.sh
@@ -49,7 +49,7 @@ GRIS="\033[0m"
 MARRON="\33[38;5;138m"
 
 echo "${BLANCO}"
-echo "  INFORMACIÓN (BM)"
+echo "  INFORMACIÓN (DMR+)"
 echo -n "${VERDE}"
 echo "  ******************************************"
 echo "${CIAN}  Indicativo:   ${AMARILLO}$indicativo   "
