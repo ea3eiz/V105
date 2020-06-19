408a44d2de3b5f2044479667c708435da55c84a7
Manel <ea3eiz@ea3eiz.com>
2020-06-19 19:19:19 +0200

    1

--- ejecutar_radio.sh
+++ ejecutar_radio.sh
@@ -27,7 +27,7 @@ cd /home/pi/Desktop
 sudo cp Abrir_Radio.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_radio.sh'" /home/pi/Abrir_Radio.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_RADIO_ON.png" /home/pi/Abrir_Radio.desktop
-sed -i "10c Name[es_ES]=Cerrar Radio" /home/pi/Abrir_Radio.desktop
+sed -i "10c Name[es_ES]=Cerrar DMO" /home/pi/Abrir_Radio.desktop
 sed -i "5c MMDVM=ON" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_Radio.desktop /home/pi/Desktop
@@ -47,7 +47,7 @@ cd /home/pi/Desktop
 sudo cp Abrir_Radio.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=80x15 -e sudo sh ejecutar_radio.sh'" /home/pi/Abrir_Radio.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_RADIO_OFF.png" /home/pi/Abrir_Radio.desktop
-sed -i "10c Name[es_ES]=Abrir Radio" /home/pi/Abrir_Radio.desktop
+sed -i "10c Name[es_ES]=Abrir DMO" /home/pi/Abrir_Radio.desktop
 sed -i "5c MMDVM=OFF" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_Radio.desktop /home/pi/Desktop
