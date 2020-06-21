5fd6eec275e4ce1fb883562a04fb0821c9c89510
Manel <ea3eiz@ea3eiz.com>
2020-06-21 11:08:49 +0200

    1

--- editar_YSF2DMR.sh
+++ editar_YSF2DMR.sh
@@ -26,7 +26,7 @@ CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
 
-linea_info_memoria="66c" #Linea del MMDVM donde guardamos el nombre de la memoria
+linea_info_memoria="55c" #Linea del MMDVM donde guardamos el nombre de la memoria
 
 echo "${VERDE}"
 echo "   **************************************************************************"
