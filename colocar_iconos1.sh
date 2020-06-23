347689bd3cdf68d606ccc461664920bc2741ba2d
Manel <ea3eiz@ea3eiz.com>
2020-06-23 13:06:22 +0200

    1

--- colocar_iconos1.sh
+++ colocar_iconos1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-cp /home/pi/icons.screen0-1904x1023.rc /home/pi/.config/xfce4/desktop
+sudo cp /home/pi/icons.screen0-1904x1023.rc /home/pi/.config/xfce4/desktop
 xfdesktop --reload
 
 
