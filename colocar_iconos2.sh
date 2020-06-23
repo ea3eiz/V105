74b81b54a1d8c68b95d43d78235b043ff15e3856
Manel <ea3eiz@ea3eiz.com>
2020-06-23 17:56:35 +0200

    1

--- colocar_iconos2.sh
+++ colocar_iconos2.sh
@@ -2,7 +2,7 @@
 sudo cp /home/pi/V105/Desktop/*.* /home/pi/Desktop
 sudo chmod 777 -R /home/pi/Desktop
 
-cp /home/pi/V105/skin2_icons.screen0-1904x1023.rc /home/pi/.config/xfce4/desktop
+cp /home/pi/V105/skin2_icons.screen0-1904x1023.rc /home/pi/.config/xfce4/desktop/icons.screen0-1904x1023.rc
 xfdesktop --reload
 
 
