2824bc71375e08b76c51fdb02eda708367874bae
Manel <ea3eiz@ea3eiz.com>
2020-06-24 09:26:19 +0200

    1

--- colocar_iconos2.sh
+++ colocar_iconos2.sh
@@ -6,7 +6,7 @@ sudo chmod 777 /home/pi/.config/xfce4/desktop/icons.screen0-1904x1023.rc
 sudo chmod 777 /home/pi/icons.screen0-1904x1023.rc
 cp /home/pi/V105/skin2_icons.screen0-1904x1023.rc /home/pi/icons.screen0-1904x1023.rc
 
-cp /home/pi/V105/SKIN2_FONDO_ADER_V105.jpg /home/pi/V105/FONDO_ADER_V105.jpg
+sudo cp /home/pi/V105/SKIN2_FONDO_ADER_V105.jpg /home/pi/V105/FONDO_ADER_V105.jpg
 sudo chmod 777 /home/pi/V105/skin2_icons.screen0-1904x1023.rc
 cp /home/pi/V105/skin2_icons.screen0-1904x1023.rc /home/pi/.config/xfce4/desktop/icons.screen0-1904x1023.rc
 
