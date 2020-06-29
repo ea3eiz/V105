bd9a9c82103c5a12a72a7353061ba10c97f9d862
Manel <ea3eiz@ea3eiz.com>
2020-06-29 12:55:27 +0200

    1

--- stop_dvswitch.sh
+++ stop_dvswitch.sh
@@ -15,6 +15,14 @@ cd /home/pi
 sudo cp Activar_dvswitch.desktop /home/pi/Desktop
 sudo rm /home/pi/Activar_dvswitch.desktop
 
+#Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+MARRON="\33[38;5;138m"
 
 clear
 echo "${BLANCO}"
