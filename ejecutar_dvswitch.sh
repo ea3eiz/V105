847cd46af781d83fe0410292971e00d6623c7aac
Manel <ea3eiz@ea3eiz.com>
2020-06-29 13:25:23 +0200

    1

--- ejecutar_dvswitch.sh
+++ ejecutar_dvswitch.sh
@@ -69,6 +69,6 @@ clear
 # echo "   ***************************************************************************"
 # sleep 3
 
-zenity --info --ok-label= --title=DVSWITCH --width=400 --timeout=3 --text "\n\nDVSWITCH ACTIVADO"
+zenity --info --ok-label= --title=DVSWITCH --window-icon=/home/pi/V105/ICONO_DVSWITCH_ON.png --width=400 --timeout=3 --text "\n\nDVSWITCH ACTIVADO"
 sed -i "18c DVSWITCH=ON" /home/pi/status.ini	
 			
