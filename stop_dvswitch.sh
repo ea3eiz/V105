f82064fcbda0fae841e10a954e55d2fa148c6b11
Manel <ea3eiz@ea3eiz.com>
2020-06-29 12:43:07 +0200

    1

--- stop_dvswitch.sh
+++ stop_dvswitch.sh
@@ -8,7 +8,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 
 cd /home/pi/Desktop
 sudo cp Activar_dvswitch.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal -e sudo sh ejecutar_dvswitch.sh'" /home/pi/Activar_dvswitch.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_dvswitch.sh'" /home/pi/Activar_dvswitch.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DVSWITCH_OFF.png" /home/pi/Activar_dvswitch.desktop
 sed -i "10c Name[es_ES]=Activar Dvswitch" /home/pi/Activar_dvswitch.desktop
 cd /home/pi
