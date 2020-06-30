67638529d2a71cb62bfc162d8ebc519e1e628951
Manel <ea3eiz@ea3eiz.com>
2020-06-30 16:27:05 +0200

    2

--- editar_MMDVMBM.sh
+++ editar_MMDVMBM.sh
@@ -358,7 +358,7 @@ numero_linea=`expr substr $Jitter 1 $largo_linea`
 Jitter=$(awk "NR==$numero_linea" $usuario/MMDVMHost/$DIRECTORIO)
 letrac=c
 numero_linea_jiter_letrac=$numero_linea$letrac
-echo "  ${CIAN}        k) ${GRIS}Jitter      - ${AMARILLO}$Jitter"
+echo "  ${CIAN}      k) ${GRIS}Jitter      - ${AMARILLO}$Jitter"
 
 
 
@@ -385,7 +385,7 @@ numero_linea=`expr $numero_linea + 1`
 FM=$(awk "NR==$numero_linea" $usuario/MMDVMHost/$DIRECTORIO)
 letra=c
 linea_sed_FM=$numero_linea$letra
-echo "${CIAN}   \t\tk) ${GRIS}FM          - ${AMARILLO}$FM"
+echo "${CIAN}   \tl) ${GRIS}FM          - ${AMARILLO}$FM"
 
 
 
