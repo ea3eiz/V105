7639d0218806d4f9ea86d1d273a4a94a015bfbcd
Manel <ea3eiz@ea3eiz.com>
2020-07-04 00:34:40 +0200

    1

--- editar_dextra.sh
+++ editar_dextra.sh
@@ -62,6 +62,14 @@ do
                         sudo chmod 777 -R /usr/share/opendv
                         cp DExtra_Hosts.txt /usr/share/opendv
                         cp DPlus_Hosts.txt /usr/share/opendv
+
+
+
+                        sudo chmod 777 -R /usr/local/share/opendv
+                        cp DExtra_Hosts.txt /usr/local/share/opendv
+                        cp DPlus_Hosts.txt /usr/local/share/opendv
+
+
                         #cambiado el 1-07-2020
                         #sudo curl --fail -o DExtra_Hosts.txt -s http://www.arrg.us/HF/DExtra_Hosts.txt
                         #sudo curl --fail -o DExtra_Hosts.txt -s http://www.pistar.uk/downloads/DExtra_Hosts.txt
