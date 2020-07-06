75e0dc82450e4f522bd3b6d805ef3a43b25919d2
Manel <ea3eiz@ea3eiz.com>
2020-07-06 14:46:54 +0200

    1

--- ejecutar_dmrgateway.sh
+++ ejecutar_dmrgateway.sh
@@ -53,6 +53,6 @@ sudo cp Abrir_dmrgateway.desktop /home/pi/Desktop
 sleep 1
 sudo rm /home/pi/Abrir_dmrgateway.desktop
 
-zenity --info  --title=DMRGateway --window-icon=/home/pi/V105/ICONO_DMRGATEWAY_ON.png --width=400 --timeout=35 --text "\nTG 8 Para hablar por DMR+\nTG 11 Para hablar por XLX"
+zenity --info --ok-label=CERRAR --title=DMRGateway --window-icon=/home/pi/V105/ICONO_DMRGATEWAY_ON.png --width=400 --timeout=35 --text "\nTG 8 Para hablar por DMR+\nTG 11 Para hablar por XLX"
 sed -i "18c DVSWITCH=ON" /home/pi/status.ini
 
