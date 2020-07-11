f0c860f680cf2b2f6a36423ee00e8c4da5a73125
Manel <ea3eiz@ea3eiz.com>
2020-07-11 11:07:37 +0200

    1

--- Actualizar_BlueDV.sh
+++ Actualizar_BlueDV.sh
@@ -22,28 +22,30 @@ echo "${VERDE}"
 echo "   ********************************************************************"
 echo "   *               Script para actualizar BlueDV                      *"
 echo -n "${ROJO}"
-echo "   *                      $SCRIPTS_version by EA3EIZ                              *"
+echo "   *                      $SCRIPTS_version by EA3EIZ                               *"
 echo -n "${VERDE}"
 echo "   ********************************************************************"
-echo "\33[1;36m   1)\33[1;37m Actualizar BlueDV"
-#echo "\33[1;36m   2)\33[1;37m Volver a la versión BlueDV Version 1.0.0.9441"
+echo "\33[1;36m   1)\33[1;37m Actualizar BlueDV Version 1.0.0.9560"
+echo "\33[1;36m   2)\33[1;37m Volver a la versión BlueDV Version 1.0.0.9441"
+#echo "\33[1;36m   2)\33[1;37m Actualizar DV4mini Control Panel (Stick ID:8D-6E-8C V1,77)"
 echo ""
-echo -n "\33[1;36m   Elige una opción: " 
+echo "\33[1;36m   0)\33[1;34m Salir del script \33[1;31m OJO!! no salir con ctrl+c ni con la x"
+echo ""
+echo -n "\33[1;36m   Elige una opción del 0 al 2: " 
 read escoger_menu
 echo ""
 case $escoger_menu in
 1) echo ""
 while true
 do
-			read a
-			clear
-			echo "\33[1;33m"
+clear
+echo "\33[1;33m"
 	        read -p ' Quieres actualizar el BlueDV Si/No: ' ejecutar1
 		    case $ejecutar1 in
 			[sS]* ) echo ""
 			echo "ok >>>>>"
-#if [ -d /home/pi/bluedv_anterior/ ];
-#then
+if [ -d /home/pi/bluedv_anterior/ ];
+then
 			clear
 			echo "\33[1;32mYA TIENES ACTUALIZADA LA ÚLTIMA VERSIÓN DISPONIBLE"
 			sleep 1
@@ -60,23 +62,23 @@ do
 			echo "\33[1;32m******************************************************"
 			echo "\33[1;32m* YA TIENES ACTUALIZADA LA ÚLTIMA VERSIÓN DISPONIBLE *"
 			echo "\33[1;32m******************************************************"
-			sleep 3
-# else
-# 			cd /home/pi/
-#   			mkdir bluedv_anterior
-#             sudo cp bluedv/*.* bluedv_anterior/
-#             sudo rm -r /home/pi/bluedv
-# 			git clone https://github.com/ea3eiz/bluedv
+			sleep 5
+else
+			cd /home/pi/
+  			mkdir bluedv_anterior
+            sudo cp bluedv/*.* bluedv_anterior/
+            sudo rm -r /home/pi/bluedv
+			git clone https://github.com/ea3eiz/bluedv
 			
-# 			cd /home/pi/bluedv
-# 			sudo chmod 777 DExtra_Hosts.txt
+			cd /home/pi/bluedv
+			sudo chmod 777 DExtra_Hosts.txt
 		
-# fi	
-			# echo "\33[1;32m******************************************************"
-			# echo "\33[1;32m*  SE HA ACTUALIZADO A LA ÚLTIMA VERSIÓN DISPONIBLE  *"
-			# echo "\33[1;32m******************************************************"
-			# sleep 3
-			exit;
+fi	
+			echo "\33[1;32m******************************************************"
+			echo "\33[1;32m*  SE HA ACTUALIZADO A LA ÚLTIMA VERSIÓN DISPONIBLE  *"
+			echo "\33[1;32m******************************************************"
+			sleep 3
+exit;
 		    echo ""
 			echo "Ok, se ha ejecutado correctamente"
 			echo ""
@@ -85,7 +87,7 @@ do
 			break;;
 esac
 done;;
-2bloqueado) echo ""
+2) echo ""
 while true
 do
 clear
