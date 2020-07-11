ffd5de3bdbd8ef1339e7b652e3d339aea4f0c0ac
Manel <ea3eiz@ea3eiz.com>
2020-07-11 10:50:36 +0200

    1

--- Actualizar_BlueDV.sh
+++ Actualizar_BlueDV.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-exit
 clear
 while true
 do
@@ -26,9 +25,8 @@ echo -n "${ROJO}"
 echo "   *                      $SCRIPTS_version by EA3EIZ                               *"
 echo -n "${VERDE}"
 echo "   ********************************************************************"
-echo "\33[1;36m   1)\33[1;37m Actualizar BlueDV Version 1.0.0.9560"
-echo "\33[1;36m   2)\33[1;37m Volver a la versión BlueDV Version 1.0.0.9441"
-#echo "\33[1;36m   2)\33[1;37m Actualizar DV4mini Control Panel (Stick ID:8D-6E-8C V1,77)"
+echo "\33[1;36m   1)\33[1;37m Actualizar BlueDV"
+#echo "\33[1;36m   2)\33[1;37m Volver a la versión BlueDV Version 1.0.0.9441"
 echo ""
 echo "\33[1;36m   0)\33[1;34m Salir del script \33[1;31m OJO!! no salir con ctrl+c ni con la x"
 echo ""
@@ -45,8 +43,8 @@ echo "\33[1;33m"
 		    case $ejecutar1 in
 			[sS]* ) echo ""
 			echo "ok >>>>>"
-if [ -d /home/pi/bluedv_anterior/ ];
-then
+#if [ -d /home/pi/bluedv_anterior/ ];
+#then
 			clear
 			echo "\33[1;32mYA TIENES ACTUALIZADA LA ÚLTIMA VERSIÓN DISPONIBLE"
 			sleep 1
@@ -63,23 +61,23 @@ then
 			echo "\33[1;32m******************************************************"
 			echo "\33[1;32m* YA TIENES ACTUALIZADA LA ÚLTIMA VERSIÓN DISPONIBLE *"
 			echo "\33[1;32m******************************************************"
-			sleep 5
-else
-			cd /home/pi/
-  			mkdir bluedv_anterior
-            sudo cp bluedv/*.* bluedv_anterior/
-            sudo rm -r /home/pi/bluedv
-			git clone https://github.com/ea3eiz/bluedv
-			
-			cd /home/pi/bluedv
-			sudo chmod 777 DExtra_Hosts.txt
-		
-fi	
-			echo "\33[1;32m******************************************************"
-			echo "\33[1;32m*  SE HA ACTUALIZADO A LA ÚLTIMA VERSIÓN DISPONIBLE  *"
-			echo "\33[1;32m******************************************************"
 			sleep 3
-exit;
+# else
+# 			cd /home/pi/
+#   			mkdir bluedv_anterior
+#             sudo cp bluedv/*.* bluedv_anterior/
+#             sudo rm -r /home/pi/bluedv
+# 			git clone https://github.com/ea3eiz/bluedv
+			
+# 			cd /home/pi/bluedv
+# 			sudo chmod 777 DExtra_Hosts.txt
+		
+# fi	
+			# echo "\33[1;32m******************************************************"
+			# echo "\33[1;32m*  SE HA ACTUALIZADO A LA ÚLTIMA VERSIÓN DISPONIBLE  *"
+			# echo "\33[1;32m******************************************************"
+			# sleep 3
+			exit;
 		    echo ""
 			echo "Ok, se ha ejecutado correctamente"
 			echo ""
