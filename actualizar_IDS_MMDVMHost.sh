fe1898c1b53b9e855131ccfc070e9cc5a54786dc
Manel <ea3eiz@ea3eiz.com>
2020-08-02 20:45:44 +0200

    1

--- actualizar_IDS_MMDVMHost.sh
+++ actualizar_IDS_MMDVMHost.sh
@@ -25,3 +25,6 @@ sed -i "$linea_sed DATABASEURL='https://ham-digital.org/status/users.csv'" /home
 
 
 sudo sh /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
+
+cp /home/pi/MMDVMHost/DMRIds.dat /var/lib/mmdvm
+
