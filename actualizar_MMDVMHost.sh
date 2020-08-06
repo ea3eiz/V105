50a08ddbd2034d1ea8b10ba0602e936eabb3a57f
Manel <ea3eiz@ea3eiz.com>
2020-08-06 09:41:49 +0200

    1

--- actualizar_MMDVMHost.sh
+++ actualizar_MMDVMHost.sh
@@ -35,6 +35,7 @@ MARRON="\33[38;5;138m"
                     cp /home/pi/MMDVMHost/MMDVMDMRGateway.ini /home/pi
                     cd /home/pi
                     sudo rm -r /home/pi/MMDVMHost
+                    sudo apt-get install build-essential git-core libi2c-dev i2c-tools lm-sensors
                     git clone https://github.com/g4klx/MMDVMHost
                     cd MMDVMHost
                     git clone https://github.com/hallard/ArduiPi_OLED
