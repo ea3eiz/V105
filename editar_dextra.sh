9652896f709ba64109bb6533bb167a8c2b6b5a4a
Manel <ea3eiz@ea3eiz.com>
2020-08-13 18:53:42 +0200

    1

--- editar_dextra.sh
+++ editar_dextra.sh
@@ -70,9 +70,10 @@ do
                         cp DCS_Hosts.txt /usr/local/share/opendv
 
                         #cambiado el 1-07-2020
+                        cd /usr/share/opendv
                         #sudo curl --fail -o DExtra_Hosts.txt -s http://www.arrg.us/HF/DExtra_Hosts.txt 
                         #sudo curl --fail -o DExtra_Hosts.txt -s http://www.pistar.uk/downloads/DExtra_Hosts.txt
-                        #sudo curl --fail -o DCS_Hosts.txt -s http://www.pistar.uk/downloads/DCS_Hosts.txt
+                        sudo curl --fail -o DCS_Hosts.txt -s http://www.pistar.uk/downloads/DCS_Hosts.txt
                         #sudo curl --fail -o DPlus_Hosts.txt -s http://www.pistar.uk/downloads/DPlus_Hosts.txt
                         
                         #ircddbgatewayd 2019
