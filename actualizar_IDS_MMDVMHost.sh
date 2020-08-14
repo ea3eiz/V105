edc9f728235a941784dcfb22705d8d2edbff08fc
Manel <ea3eiz@ea3eiz.com>
2020-08-14 14:41:31 +0200

    1

--- actualizar_IDS_MMDVMHost.sh
+++ actualizar_IDS_MMDVMHost.sh
@@ -1,9 +1,19 @@
 ﻿#!/bin/bash
+#Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+MARRON="\33[38;5;138m"
+NEGRO="\33[0;30m"
 clear
-echo "\33[1;32mACTUALIZANDO IDS INDICATIVOS"
-echo "\33[1;32mACTUALIZANDO DMRIds.dat para DVSWITCH"
+echo ""
+echo "${AMARILLO}ACTUALIZANDO IDS INDICATIVOS"
+echo "${VERDE}ACTUALIZANDO DMRIds.dat para DVSWITCH"
 sleep 10
-echo "\33[0;30m"
+echo "${NEGRO}"
 var=`grep -n -m 1 '\<DMRIDPATH\>' /home/pi/MMDVMHost/linux/DMRIDUpdate.sh`
 buscar=":"
 largo_linea=`expr index $var $buscar`
