cdea952d96e2c0a728a1f9ec82512798e71973a0
Manel <ea3eiz@ea3eiz.com>
2020-09-11 18:42:33 +0200

    1

--- actualizar_IDS_MMDVMHost.sh
+++ actualizar_IDS_MMDVMHost.sh
@@ -43,3 +43,6 @@ sudo sh /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
                         sudo curl --fail -o DMRIds.dat -s http://www.pistar.uk/downloads/DMRIds.dat
                         sudo chmod 777 -R /var/lib/mmdvm
 
+                        cd /home/pi/DMR2YSF/
+                        sudo curl --fail -o DMRIds.dat -s http://www.pistar.uk/downloads/DMRIds.dat
+
