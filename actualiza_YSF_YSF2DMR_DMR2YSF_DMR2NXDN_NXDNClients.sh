f3779b2ce83daea28f90aba95806b81325265f55
Manel <ea3eiz@ea3eiz.com>
2020-09-17 12:10:52 +0200

    1

--- actualiza_YSF_YSF2DMR_DMR2YSF_DMR2NXDN_NXDNClients.sh
+++ actualiza_YSF_YSF2DMR_DMR2YSF_DMR2NXDN_NXDNClients.sh
@@ -28,6 +28,7 @@ MARRON="\33[38;5;138m"
                         cd YSFClients
                         cd YSFGateway
                         sudo sed -i '209c std::string name = "IMAGEN ADER V105";' YSFGateway.cpp
+                        sudo sed -i '28c const char* YSF_VERSION = "ADER V105";' YSFNetwork.cpp
                         make
                         # sudo make install
                         # sudo rm *.cpp
