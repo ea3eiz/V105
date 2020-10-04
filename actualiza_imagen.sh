d047ee6963fc00defff37fd7a6883ade4d0514b1
Manel <ea3eiz@ea3eiz.com>
2020-10-04 18:12:29 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -75,17 +75,17 @@ sleep 2
                         sudo cp DExtra_Hosts.txt $usuario/dv4mini/xref.ip
 
 
-                        # Cambio 01-10-2020 para que funcione el cambiar de sistemas desde la app dvswitch
+                        # Cambio 04-10-2020 para que funcione el cambiar de sistemas desde la app dvswitch
                         sudo cp $usuario/$SCRIPTS_version/ab-restart.sh /usr/local/sbin/
                         sudo cp $usuario/$SCRIPTS_version/call_id.sh /usr/local/sbin/
                         sudo cp $usuario/$SCRIPTS_version/ssz.sh /usr/local/sbin/
                         sudo cp $usuario/$SCRIPTS_version/test.sh /usr/local/sbin/
 
-                        sudo chmod +x ab-restart.sh
-                        sudo chmod +x call_id.sh
-                        sudo chmod +x ssz.sh
-                        sudo chmod +x test.sh
-                        sudo chmod +x tune.sh
+                        sudo chmod +x /usr/local/sbin/ab-restart.sh
+                        sudo chmod +x /usr/local/sbin/call_id.sh
+                        sudo chmod +x /usr/local/sbin/ssz.sh
+                        sudo chmod +x /usr/local/sbin/test.sh
+                        sudo chmod +x /usr/local/sbin/tune.sh
 
 
                         #Lee el fichero INFO_RXF para poner los datos en los iconos INFO TXF 
