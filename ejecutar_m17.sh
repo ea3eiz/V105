afecbf763ec2c95adf9acab605ad900ef2b86767
Manel <ea3eiz@ea3eiz.com>
2020-10-23 13:25:57 +0200

    1

--- ejecutar_m17.sh
+++ ejecutar_m17.sh
@@ -2,16 +2,16 @@
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp Abrir_dv4mini.desktop /home/pi
-sed -i "4c DV4mini=ON" /home/pi/status.ini
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_dv4mini.sh'" /home/pi/Abrir_dv4mini.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/DV4_ON.png" /home/pi/Abrir_dv4mini.desktop
-sed -i "10c Name[es_ES]=Cerrar DV4mini" /home/pi/Abrir_dv4mini.desktop
-sed -i "4c DV4mini=ON" /home/pi/status.ini
+sudo cp top.desktop /home/pi
+#sed -i "4c DV4mini=ON" /home/pi/status.ini
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_m17.sh'" /home/pi/top.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_M17_ON.png" /home/pi/top.desktop
+sed -i "11c Name[es_ES]=Cerrar M17" /home/pi/top.desktop
+#sed -i "4c DV4mini=ON" /home/pi/status.ini
 cd /home/pi
-sudo cp Abrir_dv4mini.desktop /home/pi/Desktop
+sudo cp top.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/pi/Abrir_dv4mini.desktop
+sudo rm /home/pi/top.desktop
 
 cd /home/pi/mvoice
 ./mvoice
