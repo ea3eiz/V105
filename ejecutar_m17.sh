5c72ba43e52f8f65f085e4eb8d9eee626c9d133f
Manel <ea3eiz@ea3eiz.com>
2020-10-23 14:23:53 +0200

    1

--- ejecutar_m17.sh
+++ ejecutar_m17.sh
@@ -12,7 +12,7 @@ cd /home/pi
 sudo cp top.desktop /home/pi/Desktop
 sleep 1
 sudo rm /home/pi/top.desktop
-zenity --info --ok-label= --title=Abriendo_BlueDB --window-icon=/home/pi/V105/ICONO_BLUEDV_ON.png --width=400 --timeout=15 --text "Próximamente podreis descargar desde la web:\nwww.associacioader.com\una nueva versión V105 (26-10-2020)\n en la cual podreis usar el sistema M17."
+zenity --warning --ok-label=CERRAR --title=DMRGateway --window-icon=/home/pi/V105/ICONO_DMRGATEWAY_ON.png --width=500 --timeout=15 --text "Próximamente podreis descargar desde la web:\nwww.associacioader.com\una nueva versión V105 (26-10-2020)\n en la cual podreis usar el sistema M17."
 
 cd /home/pi/mvoice
 ./mvoice
