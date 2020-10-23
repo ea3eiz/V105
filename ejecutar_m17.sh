10fd3986c0ab4edc3d01a7e5313995f86567b70e
Manel <ea3eiz@ea3eiz.com>
2020-10-23 16:33:07 +0200

    1

--- ejecutar_m17.sh
+++ ejecutar_m17.sh
@@ -13,12 +13,7 @@ sudo cp top.desktop /home/pi/Desktop
 sleep 1
 sudo rm /home/pi/top.desktop
 
-			if [ -d /home/pi/mvoice/ ];
-			then
-
 zenity --warning --ok-label=CERRAR --title=Sistema_M17 --window-icon=/home/pi/V105/ICONO_DMRGATEWAY_ON.png --width=500 --text "Próximamente podreis descargar desde la web:\nhttp://www.associacioader.com\nuna nueva versión V105 (26-10-2020)\ncon la cual podréis usar este sistema M17."
-else
-#echo "no presenta el mensaje"
-fi
+
 cd /home/pi/mvoice
 ./mvoice
