173008627f86e377cce1637ea343438fe4c817e1
Manel <ea3eiz@ea3eiz.com>
2020-10-23 13:33:27 +0200

    1

--- cerrar_m17.sh
+++ cerrar_m17.sh
@@ -5,7 +5,7 @@ cd /home/pi/Desktop
 sudo cp top.desktop /home/pi
 sleep 1
 sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_m17.sh'" /home/pi/top.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_M17-OFF.png" /home/pi/top.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_M17_OFF.png" /home/pi/top.desktop
 sed -i "11c Name[es_ES]=Abrir M17" /home/pi/top.desktop
 #sed -i "4c DV4mini=OFF" /home/pi/status.ini
 sleep 1
