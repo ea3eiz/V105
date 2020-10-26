452c12ec415eb424de97e8706697e8f30dd3576e
Manel <ea3eiz@ea3eiz.com>
2020-10-26 19:35:41 +0100

    1

--- ejecutar_m17.sh
+++ ejecutar_m17.sh
@@ -19,6 +19,6 @@ sudo rm /home/pi/top.desktop
             ./mvoice
             else
             cd /home/pi/V105
-            ./avisom17
+            ./aviso
 #zenity --warning --ok-label=CERRAR --title=Sistema_M17 --window-icon=/home/pi/V105/ICONO_DMRGATEWAY_ON.png --width=500 --text "\nYa está disponible la versión ADER V105 (25-10-2020) M17\npuedes descargarla de la web:\nhttp://www.associacioader.com\n\ncon la nueva versión ya puedes usar el Sistema M17."
             fi
