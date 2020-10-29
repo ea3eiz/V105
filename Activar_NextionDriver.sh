3f84d98dc335d2b6550cb66fa4ddb99f4867f72c
Manel <ea3eiz@ea3eiz.com>
2020-10-29 01:39:41 +0100

    1

--- Activar_NextionDriver.sh
+++ Activar_NextionDriver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/home/pi/V105/./activa_nextiondriver
+/home/pi/V105/./nextiondriver_activar
 # path usuario
 usuario="/home/pi"
 usuario=$(awk "NR==1" $usuario/.config/autostart/usuario)
