1282677f083ad4ae650504c19d26402dda163c89
Manel <ea3eiz@ea3eiz.com>
2020-10-30 23:35:03 +0100

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -41,7 +41,7 @@ echo " ************************    ABRIENDO BRANDMEISTER    ********************
 echo " ******************************************************************************"
 sleep 2
 sudo ./MMDVMBM MMDVMBM.ini
-
+sudo sh /home/pi/$SCRIPTS_version/info_bm.sh
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMBM.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=80x12 -e sudo sh ejecutar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
