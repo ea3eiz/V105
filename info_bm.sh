f5a3bf3f07dc5751a87c1aa9323f59abe17b773c
Manel <ea3eiz@ea3eiz.com>
2020-10-31 20:51:53 +0100

    1

--- info_bm.sh
+++ info_bm.sh
@@ -57,52 +57,52 @@ sudo sed -i "9c Memoria:      $memoria" /home/pi/.local/memoria_bm
 
 /home/pi/V105/./qt_info_bm
 
-#Colores
-ROJO="\033[1;31m"
-VERDE="\033[1;32m"
-BLANCO="\033[1;37m"
-AMARILLO="\033[1;33m"
-CIAN="\033[1;36m"
-GRIS="\033[0m"
-MARRON="\33[38;5;138m"
-
-echo "${BLANCO}"
-echo "  INFORMACIÓN (BRANDMEISTER)"
-echo -n "${VERDE}"
-echo "  ******************************************"
-echo "${CIAN}  Indicativo:   ${AMARILLO}$indicativo   "
-echo "${CIAN}  Id:           ${AMARILLO}$id   "
-echo "${CIAN}  RXFrecuencia: ${AMARILLO}$rxfrecuencia   "
-echo "${CIAN}  TXFrecuencia: ${AMARILLO}$txfrecuencia   "
-echo "${CIAN}  Puerto:     ${AMARILLO}$puerto   "
-echo "${CIAN}  Servidor:   ${AMARILLO}$address   "
-echo "${CIAN}  Memoria:      ${AMARILLO}$memoria   "
-echo -n "${VERDE}"
-echo "  ******************************************"
-
-echo -n "${CIAN}  Pulsa enter para cerrar esta ventana "
-read a
-
-# echo -n "${MARRON}"
-# echo -n "  Se cerrará en 10."
-# sleep 1
-# echo -n "9."
-# sleep 1
-# echo -n "8."
-# sleep 1
-# echo -n "7."
-# sleep 1
-# echo -n "6."
-# sleep 1
-# echo -n "5."
-# sleep 1
-# echo -n "4."
-# sleep 1
-# echo -n "3."
-# sleep 1
-# echo -n "2."
-# sleep 1
-# echo -n "1."
-# sleep 1
-# echo -n "0"
-# sleep 1
+##Colores
+#ROJO="\033[1;31m"
+#VERDE="\033[1;32m"
+#BLANCO="\033[1;37m"
+#AMARILLO="\033[1;33m"
+#CIAN="\033[1;36m"
+#GRIS="\033[0m"
+#MARRON="\33[38;5;138m"
+#
+#echo "${BLANCO}"
+#echo "  INFORMACIÓN (BRANDMEISTER)"
+#echo -n "${VERDE}"
+#echo "  ******************************************"
+#echo "${CIAN}  Indicativo:   ${AMARILLO}$indicativo   "
+#echo "${CIAN}  Id:           ${AMARILLO}$id   "
+#echo "${CIAN}  RXFrecuencia: ${AMARILLO}$rxfrecuencia   "
+#echo "${CIAN}  TXFrecuencia: ${AMARILLO}$txfrecuencia   "
+#echo "${CIAN}  Puerto:     ${AMARILLO}$puerto   "
+#echo "${CIAN}  Servidor:   ${AMARILLO}$address   "
+#echo "${CIAN}  Memoria:      ${AMARILLO}$memoria   "
+#echo -n "${VERDE}"
+#echo "  ******************************************"
+#
+#echo -n "${CIAN}  Pulsa enter para cerrar esta ventana "
+#read a
+#
+## echo -n "${MARRON}"
+## echo -n "  Se cerrará en 10."
+## sleep 1
+## echo -n "9."
+## sleep 1
+## echo -n "8."
+## sleep 1
+## echo -n "7."
+## sleep 1
+## echo -n "6."
+## sleep 1
+## echo -n "5."
+## sleep 1
+## echo -n "4."
+## sleep 1
+## echo -n "3."
+## sleep 1
+## echo -n "2."
+## sleep 1
+## echo -n "1."
+## sleep 1
+## echo -n "0"
+## sleep 1
