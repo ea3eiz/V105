fc523b84c72de13ef8b957ee2f532a213fd56f54
Manel <ea3eiz@ea3eiz.com>
2020-11-01 00:46:23 +0100

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -43,7 +43,7 @@ echo " *************************************************************************
 
 
 sleep 2
-sudo ./MMDVMBM MMDVMBM.ini | /home/pi/V105/./qt_info_bm
+sudo ./MMDVMBM MMDVMBM.ini & /home/pi/V105/./qt_info_bm
 
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMBM.desktop /home/pi
