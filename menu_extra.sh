9926cc5363e312c599ad925a9aaeb0c9417ffd9a
Manel <ea3eiz@ea3eiz.com>
2020-11-03 17:34:43 +0100

    1

--- menu_extra.sh
+++ menu_extra.sh
@@ -724,7 +724,7 @@ clear
                                 cp /home/pi/V105/ABRIR_DMR2M17.desktop /home/pi/Desktop
                                 cp /home/pi/V105/Editar_DMR2M17.desktop /home/pi/Desktop
 
-                                cp /home/pi/V105/FONDO_ADER_V105_M17.jpg /home/pi/home/V105/FONDO_ADER_V105.jpg
+                                cp /home/pi/V105/FONDO_ADER_V105_M17.jpg /home/pi/V105/FONDO_ADER_V105.jpg
 
                                 echo ""
                                 exit;
