ea6f47548e9c89033a5275d18b8823f1fc0645ec
Manel <ea3eiz@ea3eiz.com>
2020-11-14 00:38:59 +0100

    1

--- info_ysf2dmr.sh
+++ info_ysf2dmr.sh
@@ -55,7 +55,7 @@ sudo sed -i "3c RXFrecuencia: $rxfrecuencia" /home/pi/.local/memoria_ysf2dmr
 sudo sed -i "4c TXFrecuencia: $txfrecuencia" /home/pi/.local/memoria_ysf2dmr
 sudo sed -i "5c Puerto:     $puerto" /home/pi/.local/memoria_ysf2dmr
 sudo sed -i "6c Servidor:   $address" /home/pi/.local/memoria_ysf2dmr
-sudo sed -i "7c Options:      $options" /home/pi/.local/memoria_ysf2dmr
+sudo sed -i "7c $options" /home/pi/.local/memoria_ysf2dmr
 sudo sed -i "8c Memoria:      $memoria" /home/pi/.local/memoria_ysf2dmr
 
 /home/pi/V105/./qt_info_ysf2dmr
