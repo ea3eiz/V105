5893221602ae52571648566b7bc6882b39b9d9fa
Manel <ea3eiz@ea3eiz.com>
2020-11-20 13:44:12 +0100

    1

--- menu_extra.sh
+++ menu_extra.sh
@@ -693,8 +693,6 @@ clear
                                 sudo chmod +x -R /home/pi/mvoice                       
                                 sudo ./start-mvoice
                                 sed -i "3c $version_a_instalar" /home/pi/info.ini
-                                sudo su
-                                sudo chmod +x -R /root/etc
                                 else
                                 /home/pi/V105/./aviso_mvoice_version
                                 fi
