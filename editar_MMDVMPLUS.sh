8375fc3b60ed11d54b6ef0afe47d653dd25226f1
Manel <ea3eiz@ea3eiz.com>
2020-12-03 19:35:05 +0100

    1

--- editar_MMDVMPLUS.sh
+++ editar_MMDVMPLUS.sh
@@ -583,7 +583,7 @@ do
                           case $actualizar in
                           [sS]* ) echo ""
 #sh Puertos_MMDVMPLUS.sh
-/home/pi/V105/./qt_puertos_nextion_dmrplus
+/home/pi/V105/./qt_PuertosDmrplus
                           break;;
                           [nN]* ) echo ""
                           break;;
@@ -1098,7 +1098,7 @@ do
                           case $actualizar in
 			                    [sS]* ) echo ""
 #sh Puertos_Nextion_PLUS.sh
-/home/pi/V105/./qt_PuertosDmrplus_Nextion
+/home/pi/V105/./qt_puertos_nextion_dmrplus
 			                    break;;
 			                    [nN]* ) echo ""
 			                    break;;
