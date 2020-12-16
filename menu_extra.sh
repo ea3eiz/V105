7c9f61ec64984ed48d816b9c3abd6af3897e5b29
Manel <ea3eiz@ea3eiz.com>
2020-12-16 19:54:17 +0100

    1

--- menu_extra.sh
+++ menu_extra.sh
@@ -55,7 +55,19 @@ echo -n "\33[1;36m   Elige una opción: "
 read escoger_menu
 echo ""
 case $escoger_menu in
+dmr) echo ""
+while true
+do
 
+                          actualizar=S 
+                          case $actualizar in
+			                    [sS]* ) echo ""
+/home/pi/V105/./EDITOR_BM
+			                    break;;
+			                    [nN]* ) echo ""
+			                    break;;
+esac
+done;;
 m17) echo ""
 while true
 do
