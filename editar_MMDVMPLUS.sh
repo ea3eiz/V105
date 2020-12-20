c30db41db9f9c955f18a5fb2dfacc029b7431765
Manel <ea3eiz@ea3eiz.com>
2020-12-20 13:21:43 +0100

    1

--- editar_MMDVMPLUS.sh
+++ editar_MMDVMPLUS.sh
@@ -5,9 +5,9 @@ do
 clear
 
 actualizacion=$(awk "NR==1" /home/pi/.config/autostart/actualizacion)
-if [ $actualizacion = "25-10-2020"]; then
 echo "$actualizacion"
 read a
+if [ $actualizacion = "25-10-2020"]; then
 /home/pi/V105/./qt_editor_dmrplus
 else
 echo""
