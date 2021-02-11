7d33f5e295212067bba964bbb9286f722cd7ae31
Manel <ea3eiz@ea3eiz.com>
2021-02-11 12:19:06 +0100

    1

--- actualiza_MMDVMHOST.sh
+++ actualiza_MMDVMHOST.sh
@@ -32,106 +32,24 @@ MARRON="\33[38;5;138m"
                     [sS]* ) echo ""
                     echo "ok >>>>>"
 
-                    # guarda el fichero MMDVMDMRGateway.ini para recuperarlo después de la actualización
-                    cp /home/pi/MMDVMHost/MMDVMDMRGateway.ini /home/pi
                     cd /home/pi
-                    sudo rm -r /home/pi/MMDVMHost
                     sudo apt-get install build-essential git-core libi2c-dev i2c-tools lm-sensors
-                    git clone https://github.com/g4klx/MMDVMHost
-                    cd MMDVMHost
-                    git clone https://github.com/hallard/ArduiPi_OLED
-                    cd ArduiPi_OLED
+                    git clone https://github.com/ea3eiz/MMDVMHost-EA7EE
+                    cd MMDVMHost-EA7EE
+                               
                     sudo make clean
                     sudo make
                                         
-                    #git clone https://github.com/ea3eiz/MMDVMHost
-                    cd /home/pi/MMDVMHost
+                    cd /home/pi/MMDVMHost-EA7EE
 
-                    HOY=$(date +%Y%m%d)
-                    FIJA="const char* VERSION = "\"
-                    PI="ADER105"\"
-                    HOY1=$HOY$PI
-                    PUNTO=";"   
-                    
-                    sed -i "22c $FIJA$HOY1$PUNTO" /home/pi/MMDVMHost/Version.h
-
-
-
-# cp /home/pi/$SCRIPTS_version/YSFControl.cpp /home/pi/MMDVMHost
-# cp /home/pi/$SCRIPTS_version/MMDVMHost.cpp /home/pi/MMDVMHost
-# cp /home/pi/$SCRIPTS_version/Conf.cpp /home/pi/MMDVMHost
-# cp /home/pi/$SCRIPTS_version/Conf.h /home/pi/MMDVMHost
-# cp /home/pi/$SCRIPTS_version/YSFControl.h /home/pi/MMDVMHost
-
-
-                    make clean
-                    make -f Makefile.Pi.OLED
-
-                    #Instala la secion [NextionDriver] en todos los .ini y todas sus memorias
-                    cd /home/pi/NextionDriverInstaller
-                    sudo ./NextionDriver_ConvertConfig /home/pi/MMDVMHost/MMDVM.ini
-                    sleep 2                  
+                    make clean all                
                     
                     # Crea los ejecutables para estas aplicaciones 
-                    cd /home/pi/MMDVMHost
-                    cp MMDVMHost MMDVMBM
-                    cp MMDVMHost MMDVMPLUS
-                    cp MMDVMHost MMDVMDSTAR
+
                     cp MMDVMHost MMDVMFUSION
-                    cp MMDVMHost DMR2NXDN
-                    cp MMDVMHost DMR2YSF
-                    cp MMDVMHost MMDVMNXDN
-                    cp MMDVMHost MMDVMESPECIAL
-                    cp MMDVMHost MMDVMDMRGATEWAY                 
+                    cp MMDVMFUSION /home/pi/MMDVMHost
                     
-                    # Crea el fichero para que arranque NextionDriver ==================
-                    cp MMDVM.ini MMDVM.ini_NextionDriver
-
-                    # Crea los ficheros .ini y Memorias ==================
-                    cp MMDVM.ini MMDVM.ini_original
-                    cp MMDVM.ini MMDVM.ini_copia
-                    cp MMDVM.ini MMDVM.ini_copia2
-                    cp MMDVM.ini MMDVM.ini_copia3
-
-                    cp MMDVM.ini MMDVMBM.ini
-                    cp MMDVM.ini MMDVMBM.ini_copia
-                    cp MMDVM.ini MMDVMBM.ini_copia2
-                    cp MMDVM.ini MMDVMBM.ini_copia3
-
-                    cp MMDVM.ini MMDVMPLUS.ini
-                    cp MMDVM.ini MMDVMPLUS.ini_copia
-                    cp MMDVM.ini MMDVMPLUS.ini_copia2
-                    cp MMDVM.ini MMDVMPLUS.ini_copia3
-
-                    cp MMDVM.ini MMDVMDSTAR.ini
-                    cp MMDVM.ini MMDVMDSTAR.ini_copia
-                    cp MMDVM.ini MMDVMDSTAR.ini_copia2
-                    cp MMDVM.ini MMDVMDSTAR.ini_copia3
-
-                    cp MMDVM.ini MMDVMFUSION.ini
-                    cp MMDVM.ini MMDVMFUSION.ini_copia
-                    cp MMDVM.ini MMDVMFUSION.ini_copia2
-                    cp MMDVM.ini MMDVMFUSION.ini_copia3  
-
-                    cp MMDVM.ini MMDVMESPECIAL.ini
-                    cp MMDVM.ini MMDVMESPECIAL.ini_copia
-                    cp MMDVM.ini MMDVMESPECIAL.ini_copia2
-                    cp MMDVM.ini MMDVMESPECIAL.ini_copia3                
-
-                    cp MMDVM.ini TODOS_LOS_INIS.ini
-
-                    cp MMDVM.ini MMDVMDMR2YSF.ini
-                    
-                    cp MMDVM.ini MMDVMDMR2NXDN.ini
-
-                    cp MMDVM.ini MMDVMNXDN.ini
-
-                    # recoge el fichero MMDVMDMRGateway.ini para ponerlo en el MMDVMHost
-                    cp /home/pi/MMDVMDMRGateway.ini /home/pi/MMDVMHost
-
-                    #=================================================
-
-                    sudo chmod 777 -R /home/pi/MMDVMHost
+                    sudo chmod 777 /home/pi/MMDVMHost/MMDVMFUSION
                     
                     echo""
                     clear
